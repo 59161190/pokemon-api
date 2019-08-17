9426e25205c42c446c8e7b053a356c7887bd42a4
Supachok Chokpanyarat <59161190@go.buu.ac.th>
2019-08-17 10:15:27 +0700

    Support generate ID new pokemon

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-
+class Pokemon {
+  constructor(name,type){
+    this.name=name
+    this.type = type
+    this.id=null
+  }
+}
 var pokemons = [
-  { name: "Vivillon", type: "Bug" },
-  { name: "Diggersby", type: "Normal" },
-  { name: "Raichu", type: "Electric" }
+  // { name: "Vivillon", type: "Bug" },
+  // { name: "Diggersby", type: "Normal" },
+  // { name: "Raichu", type: "Electric" }
 ];
+pokemons.push(createnewPokemon('Vivillon','Bug'))
 
 app.get("/", (req, res) => res.send("Hello World!! eeeeee"));
 // Get ->   /pokemons -> list all pokemon
@@ -19,8 +26,21 @@ app.post("/pokemons", (req, res) => {
   //req ย่อมาจาก request , res ย่อมาจาก respone การตอบกลับ
 //   console.log(req.body);
 //   res.send("Still work in progress...");
-    pokemons.push(req.body)
+    pokemons.push(createnewPokemon(req.body.name,req.body.type))
     res.sendStatus(201)
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+
+function createnewPokemon(name,type){
+  let p = new Pokemon (name,type)
+  p.id = genNewId(pokemons.length)
+  return p
+}
+
+function genNewId(num){
+    let newId = num +1
+    return newId
+}
